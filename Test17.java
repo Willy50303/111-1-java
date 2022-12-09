c3e7051caef2238ef5e49003b2ae0273d2d430c1
Willy50303 <114204281+Willy50303@users.noreply.github.com>
2022-12-09 10:43:59 +0800

    Add files via upload

---
+++ Test17.java
@@ -0,0 +1,24 @@
+public class User {
+	public static void main (String[] args) {
+		int[] arr = {7, 3, 4, 9, 6};
+		int count = 0;
+		int temp;
+		for (int x=0; x < arr.length-1; x++) { // x=0~3
+			for (int y=0; y < arr.length-1; y++) { // y=0~3
+				if (arr[y] > arr[y+1]) {
+				temp = arr[y];
+				arr[y] = arr[y+1];
+				arr[y+1] = temp;
+				}
+				count++;
+			}
+		}
+		
+		
+	for (int x=0; x<arr.length; x++){
+	     System.out.println(">>" + arr[x]);
+	} 
+	public static void add(int x){
+		System.out.println("===>" + x);
+	}
+}
